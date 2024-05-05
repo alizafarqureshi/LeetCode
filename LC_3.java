26ee2c9134af6ae6e63a680710e1721c35be31c3
alizafarqureshi <80305341+alizafarqureshi@users.noreply.github.com>
2024-05-04 21:11:09 -0400

    Create LC_3.java

---
+++ LC_3.java
@@ -0,0 +1,45 @@
+public class LC_3 {
+    public static void main(String[] args) {
+        int[] nums1={1,3};
+        int[] nums2={2};
+        int length = nums2.length + nums1.length;
+        int[] newArr = new int [length];
+        int pos= 0;
+
+        for (int i : nums1){
+            newArr[pos] = i;
+            pos++;
+        }
+
+        for(int i: nums2){
+            newArr[pos] = i;
+            pos++;
+        }
+
+        int temp = 0;
+
+        for (int i= 0; i<newArr.length; i++){
+            for (int j = i +1 ; j< newArr.length;j++){
+                if(newArr[i] > newArr[j]) {
+                    temp = newArr[i];
+                    newArr[i] = newArr[j];
+                    newArr[j] =temp;
+                }
+            }
+        }
+
+        int indexOfMedian;
+        float med;
+        if(newArr.length % 2 == 0){
+            indexOfMedian= newArr.length/2;
+            med = newArr[indexOfMedian] + (newArr[indexOfMedian - 1]);
+            med = med / 2;
+        }else {
+            indexOfMedian = newArr.length/2;
+            med = newArr[indexOfMedian];
+        }
+
+        System.out.println(med);
+
+    }
+}
